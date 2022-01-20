141c51e3db52cfbba3c38180f2215490bb8c6c1d
plexx-dev <simperlyofficial@gmail.com>
2022-01-20 20:21:31 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     const RAM_SIZE: usize = 255;
 
-    let mut ram: [f64; RAM_SIZE] = [0.0; RAM_SIZE];   
+    let ram: [f64; RAM_SIZE] = [0.0; RAM_SIZE];   
     println!("{:?}", ram);
 }
