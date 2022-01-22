6d22007a4ccd1d84442b25857242f2948d936b13
plexx-dev <simperlyofficial@gmail.com>
2022-01-22 23:57:54 +0100

    added test kinda

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,14 @@ mod vm_data;
 fn main() {
     vm::run(Path::new("files/script.rsbf"));
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use crate::vm;
+
+    #[test]
+    fn test() {
+        vm::run(Path::new("files/script.rsbf"));
+    }
+}
